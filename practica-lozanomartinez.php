8c2c223ddf44237111eeb8302f9c01503aed1f9c
Adrián Lozano Martínez <62968213+AdrianLozanoMartinez@users.noreply.github.com>
2022-04-27 19:48:41 +0200

    Subido desde NetBeans 27/04/2022

---
+++ practica-lozanomartinez.php
@@ -0,0 +1,35 @@
+<?php
+	/**
+     	  * Función que realiza suma, resta o multiplicación, según indiquemos por parámetro, dos números introducidos por parámetro y devuelve su resultado
+	  * @param double $num1 primer número
+	  * @param double $num2 segundo número
+	  * @param string $operacion operación a realizar
+	  * @return double $resultado devuelve el resultado de las operaciones
+	  * @internal
+	  * @author Adrián Lozano Martínez
+	  * @version abril 2022
+	*/
+function operaciones($num1, $num2, $operacion) {
+  $resultado = 0;
+  if ($operacion == "Suma") {
+    $resultado = $num1 + $num2;
+  } else if ($operacion == "Resta") {
+       $resultado = $num1 - $num2;
+  } else if ($operacion == "Multiplicacion") {
+    $resultado = $num1 * $num2;
+  }
+  return $resultado;
+}
+
+	/**
+     	  * Función que devuelve el cuadrado de un numero introducido como parámetro.
+     	  * @param int $numero parámetro introducido
+     	  * @return double cuadrado del número
+     	  * @author Adrián Lozano Martínez
+     	  * @version abril 2022
+   	*/
+function cuadrado($num) {
+  return $num * $num;
+}
+
+?>
